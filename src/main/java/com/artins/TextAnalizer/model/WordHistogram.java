8d15b6bbbf6dba75feddbbbc07190c089e4ee396
Martins Animasaun <martins.animasaun@ha.ax>
2019-12-08 10:23:11 +0200

    Using a synchronized list

--- src/main/java/com/artins/TextAnalizer/model/WordHistogram.java
+++ src/main/java/com/artins/TextAnalizer/model/WordHistogram.java
@@ -1,14 +1,14 @@
 package com.artins.TextAnalizer.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class WordHistogram {
-	private List<WordScore> histogram = new ArrayList<>();
+	private List<WordScore> histogram = Collections.synchronizedList(new ArrayList<>());
 	
 	public void append(List<WordScore> wordScore) {
 		histogram.addAll(wordScore);
